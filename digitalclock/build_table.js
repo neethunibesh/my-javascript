d0f5b6566c7a03eadf87ae1df515e9340a35336e
neethunibesh <neethukrishnakcva @gmail.com>
2023-01-21 13:23:26 +0530

    first commit

---
+++ digitalclock/build_table.js
@@ -0,0 +1,11 @@
+<script>
+    
+    let add=[
+
+    { Name : "meenu", To,pic:"java", starting_time:3 ,ending_time:4 },
+    { Name:"seenu" ,Topic:"c++" ,starting_time:10 ,ending_time:11},
+    { Name : "leenu" ,Topic:"php", starting_time:11 ,ending_time:12 },
+    { Name : "cheenu" ,Topic:"java script", starting_time:3 ,ending_time:4},
+    ];
+    
+</script>
