f92e906f002ad5a6937da672b29a8e1ef0de76ba
neethunibesh <neethukrishnakcva @gmail.com>
2023-01-28 15:11:13 +0530

    first

---
+++ random color generator/random.js
@@ -0,0 +1,17 @@
+function getRandomColor()
+{
+    var letter = '0123456789ABCDEFGHIJ';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letter[Math.floor(Math.random() *16)];
+    }
+    return color;
+  }
+
+  function generate() {
+    $(".color").css("background-color", getRandomColor());
+    $(".main").css("background-color", getRandomColor());
+    $("h1").css("background-color", getRandomColor());
+    $("h1").css("color", getRandomColor());
+  }
+
